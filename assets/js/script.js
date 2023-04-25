3e11044fa0a9081262041b7d74e3cf3be99df008
lukecdev <courtney.luke@outlook.com>
2023-04-25 22:35:32 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
 //DOM elements
+let quizArea = document.getElementById('quiz-area');
+let displayQuestions = document.getElementById('question-area');
+let optionA = document.getElementById('answer-a');
+let optionB = document.getElementById('answer-b');
+let optionC = document.getElementById('answer-c');
+
+let questions = [
+    {
+        question: "Which mountain is located in kerry and has a height of 998mts?",
+        option1: 'Mt.Brandon',
+        option2: 'Mt.Patrick',
+        option3: 'Mt.Alps',
+        Answer: 1,
+        
+    },
+]
