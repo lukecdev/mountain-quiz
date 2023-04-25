7559ca690b014f9e3319d403e728c52f10b1fd57
lukecdev <courtney.luke@outlook.com>
2023-04-25 20:07:54 +0100

    Create question.js

---
+++ assets/js/question.js
@@ -0,0 +1,32 @@
+//questions for the mountain quiz
+
+const quizQuestions = [{
+    question: "Which mountain is located in kerry and has a height of 998mts?",
+    answers: [
+        {option: 'Mt.Brandon'},
+        {option: 'Mt.Patrick'},
+        {option: 'Mt.Alps'},
+    ],
+    correctAnswer: 'Mt.Brandon'
+},
+{
+    question: "Which mountain is located in kerry and has a height of 1000mts?",
+    answers: [
+        {option: 'Mt.Brandon'},
+        {option: 'Mt.Patrick'},
+        {option: 'Mt.Alps'},
+    ],
+    correctAnswer: 'Mt.Brandon' 
+}
+
+
+
+
+
+
+
+
+
+
+
+}];
