e0c5e12087b5a99ef7409118f7204ad2194531b4
lukecdev <courtney.luke@outlook.com>
2023-04-26 16:39:39 +0100

    Update question.js

--- assets/js/question.js
+++ assets/js/question.js
@@ -1,32 +1,28 @@
 //questions for the mountain quiz
 
-const quizQuestions = [{
-    question: "Which mountain is located in kerry and has a height of 998mts?",
-    answers: [
-        {option: 'Mt.Brandon'},
-        {option: 'Mt.Patrick'},
-        {option: 'Mt.Alps'},
-    ],
-    correctAnswer: 'Mt.Brandon'
-},
-{
-    question: "Which mountain is located in kerry and has a height of 1000mts?",
-    answers: [
-        {option: 'Mt.Brandon'},
-        {option: 'Mt.Patrick'},
-        {option: 'Mt.Alps'},
-    ],
-    correctAnswer: 'Mt.Brandon' 
-}
-
-
-
-
-
-
-
-
-
-
-
-}];
+const questions = [
+    {
+        question: "Which mountain is located in kerry and has a height of 1998mts?",
+        option1: 'Mt.Brandon',
+        option2: 'Mt.Patrick',
+        option3: '22Mt.Alps',
+        answer: 1,
+        
+    },
+     {
+        question: "Which mountain is located in kerry and has a height of 2998mts?",
+        option1: 'Mt.Brandon',
+        option2: 'Mt.Patrick',
+        option3: '33Mt.Alps',
+        answer: 1,
+        
+    },
+     {
+        question: "Which mountain is located in kerry and has a height of 3998mts?",
+        option1: 'Mt.Brandon',
+        option2: 'Mt.Patrick',
+        option3: 'Mt.Alps',
+        answer: 1,
+        
+    },
+];
